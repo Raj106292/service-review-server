7b3f3a4a2e762310ac0a016d35c9300380abdeda
Raj106292 <rajdas.106292@gmail.com>
2022-11-08 00:00:28 +0600

    service data added

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.tkcvter.mongodb.net/?retryWrites=true&w=majority`;
-// console.log(uri);
-
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 const run = async() => {
